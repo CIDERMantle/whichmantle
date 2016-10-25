518184bf0cb462ea8e296ffe1601226e0837152b
Shi <shsim@ucsd.edu>
2016-10-25 16:56:26 -0700

    Add the function interpVsdepth.m to interp/extrap the depth slices for the predicted Vs data to get the same depth slices as the Vs obs depths.

---
+++ Data/interpVsdepth.m
@@ -0,0 +1,10 @@
+% %%%%%%%%%%%%%%%%%
+% 
+% Cider mantle: to interp/extrap to get the same depth slices for Vs obs. 
+% 
+% %%%%%%%%%%%%%%%%%
+
+function vsnew = interpVsdepth(Vs_all);
+for i = 1:4356
+vsnew(i,:) = interp1(5000:2000:201000,Vs_all(i,1:99),0:5000:200000,'linear','extrap');
+end
